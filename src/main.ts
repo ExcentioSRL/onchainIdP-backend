1487f87a3e102a3014a181964a8f310380562b07
Matteo  Marengo <matteo.marengo@excentio.com>
2023-08-04 14:32:27 +0200

    porta 4000

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('BETesi')
-    .setDescription('BETesi API description')
-    .setVersion('1.0')
-    .build();
+    const config = new DocumentBuilder()
+        .setTitle('BETesi')
+        .setDescription('BETesi API description')
+        .setVersion('1.0')
+        .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs/api/v1', app, document);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs/api/v1', app, document);
 
-  await app.listen(8080);
+    await app.listen(4000);
 }
 bootstrap();
