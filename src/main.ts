d6b0b2d947cc6163613755fba6d3792a99015230
Matteo  Marengo <matteo.marengo@excentio.com>
2023-08-04 15:23:32 +0200

    aggiunto enable cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('docs/api/v1', app, document);
 
+    app.enableCors();
     await app.listen(4000);
 }
 bootstrap();
