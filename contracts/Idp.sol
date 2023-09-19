47cda5fc6575574e0f482f7d77cfc7096d6514b6
Matteo  Marengo <matteo.marengo@excentio.com>
2023-09-19 22:43:04 +0200

    fix su addRent aggiunti controlli e funzione per controllare lo stato dell'account

--- contracts/Idp.sol
+++ contracts/Idp.sol
@@ -29,6 +29,10 @@ contract Idp {
     event CheckValue(uint key, bool isValid);
 
     event CheckRent(RentalStruct r);
+
+    event CheckBooleanValue(bool b);
+
+    event CheckRentKey(uint key, bool valid);
       
     /* ------------- FINE EVENTI PER DEBUG ------------ */
 
@@ -93,7 +97,7 @@ contract Idp {
  
 
     /*  cap e reward servono per il token */
-    constructor(uint256 cap, uint256 reward) {
+    constructor(uint cap, uint reward) {
         tokenExc = new Excentio(cap,reward);
         userNumber = 0;
     }
@@ -195,10 +199,31 @@ contract Idp {
         userNumber++;
     }
 
- 
+    function getPrivateUserData() public view returns(PrivateUserData[] memory){ 
+        PrivateUserData[] memory result = new PrivateUserData[](userNumber);
 
+		for (uint index = 0; index < userNumber; index++) {
+		        result[index].platforms = new PlatformStruct[](userData[index].platformNumber);
+				result[index].userAddr = userData[index].userAddr;
+                result[0].rentals = new RentalStruct[](userData[index].allRentalsNumber);
+      
+                for(uint i = 0; i < userData[index].platformNumber; i++){
+                    result[index].platforms[i] = userData[index].platforms[i];
+                }
+               
+                uint j = 0;
+                for(uint i = 0; i < userData[index].rentalsNumber; i++){
+                    uint totRent = userData[index].rentals[i].length;
+                    for(uint k = 0; k < totRent; k++){
+                        result[0].rentals[j] = userData[index].rentals[i][k];
+                        j++;
+                    }
+                }
+		}
 
-
+		return result;
+    }
+   
     function getPrivateUserDataById(address user) public view returns(PrivateUserData[] memory){ 
         PrivateUserData[] memory result = new PrivateUserData[](1);
         
@@ -212,10 +237,7 @@ contract Idp {
         for(uint i = 0; i < userData[index].platformNumber; i++){
             result[0].platforms[i] = userData[index].platforms[i];
         }
-               
-
-        result[0].rentals = new RentalStruct[](userData[index].allRentalsNumber);
-      
+                     
         uint j = 0;
         for(uint i = 0; i < userData[index].rentalsNumber; i++){
             uint totRent = userData[index].rentals[i].length;
@@ -232,42 +254,83 @@ contract Idp {
 
     /* ------------------ INIZIO PARTE NOLEGGIO ------------------ */
 
+    function isSlotBusy (uint time, uint sd, uint ed) internal pure returns(bool){
+        if (time >= sd){
+            if(time <= ed)
+                return false;
+            else
+                return true;     
+        }
+        else  return true;
+    }
+
+    function checkRent(uint timestamp, uint time, address userAddress) public view returns(bool) {
+        uint userKey = getUserKey(userAddress);
+
+    
+        if(userData[userKey].rentalsKey[timestamp].valid == false)
+            return true;
+        
+        uint rentalKey = userData[userKey].rentalsKey[timestamp].key;
+
+        uint length = userData[userKey].rentals[rentalKey].length;
+
+        for(uint i = 0; i < length; i++){
+            bool value = isSlotBusy(time,userData[userKey].rentals[rentalKey][i].start,userData[userKey].rentals[rentalKey][i].end);
+            require(value == true, "Al momento il tuo account e' noleggiato, non puoi accederci") ;
+        }
+
+        return true;
+    }
+
+    function isSlotAvailable (uint isd, uint ied, uint sd, uint ed) internal pure returns(bool){
+        if (isd >= sd && isd <= ed)
+            return false;
+        if (ied >= sd && ied <= ed)
+            return false;
+        if (isd <= sd && ed <= ied)
+            return false;
+
+        return true;
+    }
+
+    function isRentValid(RentalStruct memory newRent, uint timestamp, uint userKey) internal view returns(bool){
+        uint rentKey = userData[userKey].rentalsKey[timestamp].key;
+
+        uint length = userData[userKey].rentals[rentKey].length;
+
+        for(uint i = 0; i < length; i++){
+            RentalStruct memory rent = userData[userKey].rentals[rentKey][i];
+            bool available = isSlotAvailable(newRent.start, newRent.end, rent.start, rent.end);
+            if(available == false)
+                return false;
+        }
+
+        return true;
+    }
 
     function addRentToUser(RentalStruct memory newRent, uint timestamp, uint userKey)  internal {
-        KeyStruct memory rent = userData[userKey].rentalsKey[timestamp];
-        
-        if(rent.valid == false){
-            rent.key = userData[userKey].rentalsNumber;
-            rent.valid = true;
+      
+        if(userData[userKey].rentalsKey[timestamp].valid == false){
+            userData[userKey].rentalsKey[timestamp].key = userData[userKey].rentalsNumber;
+            userData[userKey].rentalsKey[timestamp].valid = true;
             userData[userKey].rentalsNumber++;
         }
 
-        userData[userKey].rentals[rent.key].push(newRent);
+        userData[userKey].rentals[userData[userKey].rentalsKey[timestamp].key].push(newRent);
         userData[userKey].allRentalsNumber++;
-        
-
        
     }
 
-
     function addRent(string memory transactionId, address renter, address hirer, uint startDate, uint endDate, uint amount, string memory platformId, uint timestamp) public   {
-        require(endDate > startDate,"La data di fine noleggio e' prima dell'inizio del noleggio");
-        require(checkUserExist(renter) == true, "L'utente non esistente");
-        require(checkUserExist(hirer) == true, "Non esisti, devi prima aggiungerti");
-        require(checkUserPlatform(renter,platformId) == true, "Piattaforma non associata all'utente");
-        require(tokenExc.balanceOf(hirer) >= amount, "La tua balance non copre il costo del noleggio");
-
-        bool result = tokenExc.transferFrom(hirer,renter, amount);
-
-        require(result == true, "Trasferimento fondi non andato a buon fine, il noleggio non e' stato approvato");
-
+        
         uint renterKey = getUserKey(renter);
         uint hirerKey = getUserKey(hirer);
-
+ 
         require(renter == userData[renterKey].userAddr, "l'indirizzo dello user passato non corrisponde a quello salvato");
         require(hirer == userData[hirerKey].userAddr, "l'indirizzo dello user passato non corrisponde a quello salvato");
-        
-        RentalStruct memory rent = RentalStruct({
+
+        RentalStruct memory newRent = RentalStruct({
             transactionId: transactionId,
             renter: renter, 
             hirer: hirer, 
@@ -278,8 +341,25 @@ contract Idp {
             timestamp: timestamp
         });
 
-        addRentToUser(rent, timestamp, hirerKey);
-        addRentToUser(rent, timestamp, renterKey);
+        bool validRenter = isRentValid(newRent, timestamp, renterKey);
+        bool validHirer = isRentValid(newRent, timestamp, hirerKey);
+        
+        require(validRenter == true, "Non e' possibile effettuare il noleggio");
+        require(validHirer == true, "Non e' possibile effettuare il noleggio");
+
+        require(endDate > startDate,"La data di fine noleggio e' prima dell'inizio del noleggio");
+        require(checkUserExist(renter) == true, "L'utente non esistente");
+        require(checkUserExist(hirer) == true, "Non esisti, devi prima aggiungerti");
+        require(checkUserPlatform(renter,platformId) == true, "Piattaforma non associata all'utente");
+        require(renter != hirer, "Non puoi noleggiare da te stesso");
+        
+        require(tokenExc.balanceOf(hirer) >= amount, "La tua balance non copre il costo del noleggio");
+        bool result = tokenExc.transferFrom(hirer,renter, amount);
+        require(result == true, "Trasferimento fondi non andato a buon fine, il noleggio non e' stato approvato");
+
+
+        addRentToUser(newRent, timestamp, hirerKey);
+        addRentToUser(newRent, timestamp, renterKey);
 
     }
 
